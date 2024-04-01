0be7755e2f5e8d156828b46dd9ee57531bf94013
Abhiabhishek5124 <78491711+Abhiabhishek5124@users.noreply.github.com>
2024-04-01 01:12:31 -0400

    optimized, tested and ready

--- Mastermind/src/gameDaemon.java
+++ Mastermind/src/gameDaemon.java
@@ -5,21 +5,25 @@ public class gameDaemon {
     private static int port = 42456, maxConnections= 0;
 
     public static void main(String[] args) {
-        int i =0;
         try{
+            // Create a ServerSocket object to listen on the specified port
+
             ServerSocket server= new ServerSocket(port);
             Socket socket;
-            while ((i++<maxConnections) || maxConnections==0) {
+            System.out.println("Waiting for connection");
 
-                System.out.println("Waiting for connection");
+            // Server loop to continuously accept client connections
+            while (true) {
+                // Accept incoming client connection
                 socket = server.accept();
-                gameThread conn= new gameThread(socket);
-                conn.start();
-//                System.out.println("Received request");
+                gameThread thread= new gameThread(socket);
+                // Start the thread to handle the client's request
+                thread.start();
+                System.out.println("Received request");
             }
-
         }
         catch (IOException e) {
+            // Catch and handle any IOException that might occur
             System.out.println("IOException occurred: " + e.getMessage());
             e.printStackTrace();
         }
--- Mastermind/src/gameThread.java
+++ Mastermind/src/gameThread.java
@@ -3,13 +3,13 @@ import java.net.Socket;
 import java.util.Random;
 
 public class gameThread extends Thread {
-    private final Socket client;
+    private Socket client;
 
     public gameThread(Socket clientSocket) {
         this.client = clientSocket;
     }
 
-    // Method to process the user's guess and generate the response
+    // Process guess method
     private static String processGuess(String guess, String code) {
         StringBuilder response = new StringBuilder();
         StringBuilder remainingGuess = new StringBuilder(guess);
@@ -31,6 +31,7 @@ public class gameThread extends Thread {
         if (response.length() < 4) { // Not all positions filled with 'P'
             for (int i = 0; i < 4; i++) {
                 char codeChar = remainingCode.charAt(i);
+                //check if characters are not equal to space, otherwise it will also count as matching
                 if (codeChar != ' ') {
                     int index = remainingGuess.indexOf(String.valueOf(codeChar));
 
@@ -41,7 +42,6 @@ public class gameThread extends Thread {
                 }
             }
         }
-
         return response.reverse().toString(); // Reverse the response for correct output order
     }
 
@@ -58,7 +58,7 @@ public class gameThread extends Thread {
             for (int i = 0; i < 4; i++) {
                 codeBuilder.append(letters.charAt(random.nextInt(letters.length())));
             }
-            System.out.println("Game string to win is: " + codeBuilder);
+            System.out.println("Game string to win is: " + codeBuilder); //print the game code
 
             int count = 0;
             while (count <= 20) { // Limit the number of guesses to 20
@@ -74,11 +74,12 @@ public class gameThread extends Thread {
                     break; // Exit the loop if the user wants to quit
                 }
                 gameString = processGuess(gameString, codeBuilder.toString()); // Process the guess
+//                gameString = processGuess(gameString, "BGWG");
                 System.out.println("The gameString is: " + gameString);
                 out.writeUTF(gameString); // Send the response to the client
             }
-
             client.close(); // Close the socket when done
+
         } catch (IOException e) {
             System.out.println("IOException on socket listen: " + e);
             e.printStackTrace();
